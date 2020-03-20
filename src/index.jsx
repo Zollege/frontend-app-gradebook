1efc3a716546d6b0e75c9fc514c18010b1b24113
Kevin Colten <kevincolten@gmail.com>
2020-03-20 01:56:01 -0500

    lowercase email

--- src/index.jsx
+++ src/index.jsx
@@ -53,7 +53,7 @@ const socialLinks = [
 function init() {
   LmsApiService.fetchCurrentUser().then((currentUsers) => {
     const [currentUser] = currentUsers.data;
-    window.LogRocket.identify(currentUser.email);
+    window.LogRocket.identify(currentUser.email.toLowerCase());
     window.LogRocket.identify(currentUser.name);
   });
 }
